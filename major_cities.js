4eef5482cedf7f95ae52cb7ced42d38c280aa832
Ela Tan <ela.tan@gmail.com>
2016-08-02 09:57:01 -0700

    major cities

---
+++ major_cities.js
@@ -0,0 +1,16 @@
+// Given a JavaScript object that looks like this:
+// var majorCities = {BC: ["Vancouver", "Victoria", "Prince George"], AB: ["Edmonton", "Calgary"]};
+// Write a piece of code that loops through the object and prints the following to the console: BC has 3 main cities AB has 2 main cities
+
+var majorCities = {BC: ["Vancouver", "Victoria", "Prince George"], AB: ["Edmonton", "Calgary"]};
+
+var array = [];
+for (var i in majorCities){
+  array.push(majorCities[i]);
+}
+
+var bcCities = array[0].length
+var abCities = array[1].length
+
+
+console.log("BC has " + bcCities + " main cities " + abCities + " AB has 2 main cities.");
